26917cd200b134ee08e3230fd3820d5575055dfe
partychen <partychen.acm@gmail.com>
2020-11-09 17:00:25 +0800

    init the project

---
+++ src/Accessors/Models/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FileShare.Accessors.Models
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+
+    }
+}
---
+++ src/WebApi/Controllers/PingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public Task<string> Ping()
+        {
+            var version = Assembly.GetEntryAssembly()
+                                  ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                  ?.InformationalVersion;
+            return Task.FromResult($"File Share Api Version:{version}\nDatetime:{DateTime.UtcNow:o}");
+        }
+    }
+}
